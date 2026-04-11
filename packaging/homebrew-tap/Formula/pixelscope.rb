15df8cdff6a0955ebaf8477c91796f200414926f
LJ <lijiangpku@gmail.com>
2026-04-11 12:20:55 -0700

    Update Homebrew formula to v0.1.4

--- packaging/homebrew-tap/Formula/pixelscope.rb
+++ packaging/homebrew-tap/Formula/pixelscope.rb
@@ -1,16 +1,14 @@
 class Pixelscope < Formula
   desc "Cross-platform pixel inspection desktop viewer"
   homepage "https://github.com/woodknight/pixel-scope"
-  url "https://github.com/woodknight/pixel-scope/releases/download/v0.1.0/pixelscope-0.1.0-macos-arm64.zip"
-  version "0.1.0"
-  sha256 "a4b54d710c4637ee9a35e33461d8cba9e4167b09484cb293b5a5bec037321e68"
+  url "https://github.com/woodknight/pixel-scope/releases/download/v0.1.4/pixelscope-0.1.4-macos-arm64.zip"
+  version "0.1.4"
+  sha256 "bb2486211a87bdbfe4f49b473433b3b5b3b25ab89534fc5e947c0d354bc19147"
   license "MIT"
-
-  depends_on "libtiff"
 
   def install
     libexec.install Dir["*"]
-    bin.write_exec_script libexec/"PixelScope-0.1.0-Darwin-arm64/bin/pixelscope"
+    bin.write_exec_script libexec/"PixelScope-0.1.4-Darwin-arm64/bin/pixelscope"
   end
 
   test do
