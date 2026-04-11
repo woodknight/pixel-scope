859d4cf510520bf8231c572a04adcd7e3f725a79
woodknight <jiangli@bu.edu>
2026-04-11 15:16:47 -0700

    Add macOS .app bundle creation and caveats to Pixelscope formula

--- packaging/homebrew/Formula/pixelscope.rb
+++ packaging/homebrew/Formula/pixelscope.rb
@@ -9,9 +9,60 @@ class Pixelscope < Formula
   def install
     libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/pixelscope"
+
+    # Create a macOS .app bundle so the app appears in Launchpad / Spotlight
+    app = prefix/"PixelScope.app/Contents"
+    (app/"MacOS").mkpath
+    (app/"Resources").mkpath
+
+    cp libexec/"share/pixelscope/icons/pixelscope.icns", app/"Resources/pixelscope.icns"
+
+    (app/"MacOS/PixelScope").write <<~SCRIPT
+      #!/bin/bash
+      exec "#{libexec}/bin/pixelscope" "$@"
+    SCRIPT
+    (app/"MacOS/PixelScope").chmod 0755
+
+    (app/"Info.plist").write <<~PLIST
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>CFBundleName</key>
+        <string>PixelScope</string>
+        <key>CFBundleDisplayName</key>
+        <string>PixelScope</string>
+        <key>CFBundleIdentifier</key>
+        <string>com.woodknight.pixelscope</string>
+        <key>CFBundleVersion</key>
+        <string>#{version}</string>
+        <key>CFBundleShortVersionString</key>
+        <string>#{version}</string>
+        <key>CFBundleExecutable</key>
+        <string>PixelScope</string>
+        <key>CFBundleIconFile</key>
+        <string>pixelscope</string>
+        <key>CFBundlePackageType</key>
+        <string>APPL</string>
+        <key>NSHighResolutionCapable</key>
+        <true/>
+      </dict>
+      </plist>
+    PLIST
+  end
+
+  def caveats
+    <<~EOS
+      To add PixelScope to your Applications folder:
+        ln -sf #{prefix}/PixelScope.app /Applications/PixelScope.app
+      To remove the link:
+        rm /Applications/PixelScope.app
+    EOS
   end
 
   test do
     assert_predicate bin/"pixelscope", :exist?
+    assert_predicate prefix/"PixelScope.app/Contents/MacOS/PixelScope", :executable?
   end
 end
